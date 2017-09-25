eb6788f3f42f0a6cab253a47de7a2c2255693cc0
侠义书生 <xiayishusheng@gmail.com>
2017-09-25 17:17:55 +0800

    add code

---
+++ python/100内的素数.py
@@ -0,0 +1,1 @@
+print(' '.join([str(item) for item in filter(lambda x: all(map(lambda p: x % p != 0, range(2, x))), range(2, 101))]))
---
+++ python/8皇后问题.py
@@ -0,0 +1,1 @@
+[__import__('sys').stdout.write('\n'.join('.' * i + 'Q' + '.' * (8-i-1) for i in vec) + "\n========\n") for vec in __import__('itertools').permutations(range(8)) if 8 == len(set(vec[i]+i for i in range(8))) == len(set(vec[i]-i for i in range(8)))]
